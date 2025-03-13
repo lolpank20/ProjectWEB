40d139292cb620ae025a6a6e73cf165984e30e40
Diego Alejandro Barrera <149225105+lolpank20@users.noreply.github.com>
2025-03-13 12:05:34 -0500

    Interaccuines y experiencia

--- script.js
+++ script.js
@@ -49,3 +49,18 @@ document.querySelectorAll('#navbar a').forEach(anchor => {
         });
     });
 });
+
+document.querySelectorAll('#navbar a').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        const targetId = this.getAttribute('href').substring(1);
+        const targetSection = document.getElementById(targetId);
+
+        if (targetSection) {
+            window.scrollTo({
+                top: targetSection.offsetTop - 60,
+                behavior: 'smooth'
+            });
+        }
+    });
+});
